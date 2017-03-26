2adb9490595fdc4d191524c904e9d8685407c083
SN0WH0USE <kylegusttafson9@gmail.com>
2017-03-25 20:28:34 -0600

    Update 03_fib_print.js

--- HW-4/03_fib_print.js
+++ HW-4/03_fib_print.js
@@ -1,20 +1,15 @@
-/*
-HW-4; part 3
-INTERACTIVE FIBONACCI PRINTING
+var a,b,res,fib;
+a=0;
+b=1;
+res=b;
 
-*/
-
-// Import readline
-var readline = require('readline');
-
-// Define the Fibonacci Algorithm Function
-function fib(num) {
-    // console.log(num);
-    if (num === 0) {
-        return 0;
-    } else if (num === 1) {
-        return 1;
-    } else {
-        return fib(num - 1) + fib(num - 2);
-    }
+for (i = 0; i < fib; i++) {
+   res + "<br>";
+    res=a+b
+    a=b;
+    b=res;
 }
+
+
+//to get a desired output set 
+fib = "x"
